2e6c9de1904f980e4ff0378923805988162c8805
Abnel <Abnel@MacBook-Air-de-Abnel.local>
2026-06-10 11:39:32 -0400

    feat(cardio): adiciona altura ao paciente para calculo de imc

---
+++ alembic/versions/62740b46b634_sync_hml_cardiometabolico.py
@@ -0,0 +1,27 @@
+"""sync hml cardiometabolico
+
+Revision ID: 62740b46b634
+Revises: fb433ae2eb57
+Create Date: 2026-06-10 10:51:50.205832
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "62740b46b634"
+down_revision: Union[str, Sequence[str], None] = "fb433ae2eb57"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "pacientes",
+        sa.Column("altura", sa.Numeric(4, 2), nullable=True),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("pacientes", "altura")
